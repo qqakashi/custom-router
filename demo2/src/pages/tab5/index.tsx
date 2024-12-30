4c939bc5dafdcd1176ca71166f35dd4fd3fd5b1c
akashi <2289005415@qq.com>
2024-12-30 09:11:44 +0800

    .

--- demo2/src/pages/tab5/index.tsx
+++ demo2/src/pages/tab5/index.tsx
@@ -81,93 +81,10 @@ export default function System() {
     },[tabIndex])
 
     return (<div style={{ display: "flex", width: "100%", height: "100%", background: "#fff", boxSizing: "border-box"}}>
-            {messageInstans}
-            <div style={{ display: "flex", flexDirection: "column", marginLeft: 12}}>
-                <div style={{height: 50, display: "flex", alignItems: "center"}}>
-                    <span style={{ fontSize: 20, fontWeight: "bold"}}>&ensp;系统设置</span>
-                </div>
-                <div onClick={()=>tabsChange("device-manage", 0)} style={{cursor: "pointer", height: 50, display: "flex", alignItems: "center", padding: "0 12px" ,marginTop: 12, borderRadius: 5, width: 275, boxSizing: "border-box", background: tabIndex==0?"rgba(63,81,181,0.05)":"#fff", boxShadow: "0px 0px 2px rgba(63,81,181,0.3)"}}>
-                    <img src={require("../../img/sbgl.png")} style={{width: 24, height: 24}} />
-                    <span style={{ fontSize: 16, flex: 1 }}>&emsp;设备管理</span>
-                    <img src={require("../../img/rto.png")} style={{width: 12, height: 12}} />
-                </div>
-                <span style={{fontSize: 12, color: "#757575", marginTop: 24, marginBottom: 12}}>数据</span>
-                <div style={{width: 275, borderRadius: 5, background: "rgba(63,81,181,0.05)", boxShadow: "0px 0px 2px rgba(63,81,181,0.3)"}}>
-                    {/* <div onClick={()=>tabsChange("data-save", 1)} style={{ display: "flex", cursor: "pointer", borderTopLeftRadius: 5, borderTopRightRadius: 5, alignItems: "center", background: tabIndex==1?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/save.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{borderBottom: "1px solid rgba(63,81,181,0.15)", marginLeft: 12, flex: 1, boxSizing: "border-box", height: 50, display: "flex", alignItems: "center"}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>数据保存</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div> */}
-                    <div onClick={()=>tabsChange("warn-set", 2)} style={{ display: "flex", cursor: "pointer", alignItems: "center", borderRadius: 5, background: tabIndex==2?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/gjsz.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{height: 50, display: "flex", flex: 1, alignItems: "center", marginLeft: 12}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>告警设置</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div>
-                    {/* <div onClick={()=>tabsChange("warn-set", 2)} style={{ display: "flex", cursor: "pointer", alignItems: "center", borderBottomLeftRadius: 5, borderBottomRightRadius: 5, background: tabIndex==2?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/gjsz.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{height: 50, display: "flex", flex: 1, alignItems: "center", marginLeft: 12}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>告警设置</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div> */}
-                </div>
-                {/* <span style={{fontSize: 12, color: "#757575", marginTop: 24, marginBottom: 12}}>协议</span>
-                <div style={{width: 275, borderRadius: 5, background: "rgba(63,81,181,0.05)", boxShadow: "0px 0px 2px rgba(63,81,181,0.3)"}}>
-                    <div onClick={()=>tabsChange("protocol-103",3)} style={{ display: "flex", cursor: "pointer", alignItems: "center", borderTopLeftRadius: 5, borderTopRightRadius: 5, background: tabIndex==3?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/xy1.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{borderBottom: "1px solid rgba(63,81,181,0.15)", marginLeft: 12, flex: 1, boxSizing: "border-box", height: 50, display: "flex", alignItems: "center"}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>103</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div>
-                    <div onClick={()=>tabsChange("protocol-PMU",4)} style={{ display: "flex", cursor: "pointer", alignItems: "center", borderBottomLeftRadius: 5, borderBottomRightRadius: 5, background: tabIndex==4?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/xy2.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{height: 50, display: "flex", flex: 1, alignItems: "center", marginLeft: 12}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>PMU</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div>
-                </div> */}
-                {/* <span style={{fontSize: 12, color: "#757575", marginTop: 24, marginBottom: 12}}>系统</span>
-                <div onClick={()=>tabsChange("individuation",5)} style={{cursor: "pointer", height: 50, display: "flex", alignItems: "center", padding: "0 12px" ,marginTop: 12, borderRadius: 5, width: 275, boxSizing: "border-box", background: tabIndex==5?"rgba(63,81,181,0.05)":"#fff", boxShadow: "0px 0px 2px rgba(63,81,181,0.3)" }}>
-                    <img src={require("../../img/gxh.png")} style={{width: 24, height: 24}} />
-                    <div style={{marginLeft: 12, flex: 1, boxSizing: "border-box", height: 50, display: "flex", alignItems: "center"}}>
-                        <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>个性化</span>
-                        <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                    </div>
-                </div> */}
-                {/* <div style={{width: 275, borderRadius: 5, background: "rgba(63,81,181,0.05)", boxShadow: "0px 0px 2px rgba(63,81,181,0.3)"}}>
-                    <div onClick={()=>tabsChange("individuation",5)} style={{ display: "flex", cursor: "pointer", alignItems: "center", borderBottomLeftRadius: 5, borderBottomRightRadius: 5, background: tabIndex==5?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/gxh.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{borderBottom: "1px solid rgba(63,81,181,0.15)", marginLeft: 12, flex: 1, boxSizing: "border-box", height: 50, display: "flex", alignItems: "center"}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>个性化</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div>
-                    <div onClick={()=>tabsChange("import-set",6)} style={{ display: "flex", cursor: "pointer", alignItems: "center", background: tabIndex==6?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/dr.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{borderBottom: "1px solid rgba(63,81,181,0.15)", marginLeft: 12, flex: 1, boxSizing: "border-box", height: 50, display: "flex", alignItems: "center"}}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>导入设置</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div>
-                    <div onClick={()=>tabsChange("export-set",7)} style={{ display: "flex", cursor: "pointer", alignItems: "center", borderBottomLeftRadius: 5, borderBottomRightRadius: 5, background: tabIndex==7?"rgba(63,81,181,0.05)":"#fff" }}>
-                        <img src={require("../../img/dc2.png")} style={{width: 24, height: 24, marginLeft: 12}} />
-                        <div style={{height: 50, display: "flex", flex: 1, alignItems: "center", marginLeft: 12 }}>
-                            <span style={{ fontSize: 16, flex: 1, color: "rgba(0,0,0,0.85)" }}>导出设置</span>
-                            <img src={require("../../img/rto.png")} style={{width: 12, height: 12, marginRight: 12}} />
-                        </div>
-                    </div>
-                </div> */}
-            </div>
-            <div style={{marginLeft: 24, flex: 1}}>
-                <SystemRouter systemRouterList={systemRouterList} systemParams={pageParams} systemNavi={systemNavi} systemBack={goBack}>
-                    <Tabs activeKey={systemRouterList[tabIndex][systemRouterList[tabIndex].length-1]?.name} items={tabs} />
-                </SystemRouter>
-            </div>
+        <div style={{marginLeft: 24, flex: 1}}>
+            <SystemRouter systemRouterList={systemRouterList} systemParams={pageParams} systemNavi={systemNavi} systemBack={goBack}>
+                <Tabs activeKey={systemRouterList[tabIndex][systemRouterList[tabIndex].length-1]?.name} items={tabs} />
+            </SystemRouter>
+        </div>
     </div>)
 }
